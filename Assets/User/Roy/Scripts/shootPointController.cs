d20f84d7323773ed69ee9b7bb2491d5a6c4fd310
roychshao <roych.shao@gmail.com>
2022-11-30 18:09:14 +0800

    bird can shoot but poor

--- Assets/User/Roy/Scripts/shootPointController.cs
+++ Assets/User/Roy/Scripts/shootPointController.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class shootPointController : MonoBehaviour
 {
+    private const bool DEBUGMODE = false;
     public Camera camera;
     public GameObject bird;
     private GameObject readyBird;
-    private float speed = 100f;
+    private float force = 15f;
     private bool canShoot = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        canShoot = true;    
+        canShoot = true;
     }
 
     // Update is called once per frame
@@ -21,10 +22,29 @@ public class shootPointController : MonoBehaviour
     {
         // invalid because the mouse see is 2D
         if(Input.GetMouseButton(0) && canShoot) {
-            Vector3 direction = Input.mousePosition - this.transform.position;
-            bird.GetComponent<Rigidbody>().velocity = direction * speed;
+            // 將座標轉換成世界座標
+            //Vector3 pos = camera.ScreenToWorldPoint(bird.transform.position);
+            Vector3 pos = bird.transform.position;
+            Vector3 mousePos = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, bird.transform.position.z));
+            mousePos.y -= 1.87f;
+
+            if(DEBUGMODE) {
+                Debug.Log("mouse position: " + mousePos);
+                Debug.Log("bird position: " + pos);
+            }
+            // 設定direction
+            Vector3 direction = new Vector3(mousePos.x - pos.x, mousePos.y - pos.y, 0f);
+            direction = direction.normalized * force;
+            direction += Vector3.forward * force;
+
+            if(DEBUGMODE)
+                Debug.Log("direction: " + direction);
+            
+            // 給readybird速度
+            readyBird.GetComponent<Rigidbody>().velocity = direction;
+            if(DEBUGMODE)
+                Debug.Log("velocity: " + readyBird.GetComponent<Rigidbody>().velocity);
             canShoot = false;
-            Debug.Log(bird.GetComponent<Rigidbody>().velocity);
         }
     }
 
